c35e483e17b982da73916f336fb44d196a873245
MiaoMilafish <2175045638@qq.com>
2026-03-11 23:20:13 +0800

    websocket代码

---
+++ src/main/java/io/github/sweetzonzi/py_port/MCWebSocketServer.java
@@ -0,0 +1,50 @@
+package io.github.sweetzonzi.py_port;
+
+import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+
+import java.net.InetSocketAddress;
+
+public class MCWebSocketServer extends WebSocketServer {
+
+    public MCWebSocketServer(int port) {
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println("Python connected");
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+
+        System.out.println("Received: " + message);
+
+        conn.send("{\"status\":\"ok\"}");
+
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+        System.out.println("Connection closed");
+
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+
+        ex.printStackTrace();
+
+    }
+
+    @Override
+    public void onStart() {
+
+        System.out.println("WebSocket server started");
+
+    }
+
+}
